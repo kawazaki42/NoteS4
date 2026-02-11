4cbda0972dbc40830930bb25a9a50cdca2ba0ccd
kawazaki42 <212337579+kawazaki42@users.noreply.github.com>
2026-02-11 22:32:02 +0900

    numerous changes to 01-opt.rb

--- comp-mat/01-opt.rb
+++ comp-mat/01-opt.rb
@@ -20,16 +20,30 @@ EPSILON = 1e-10
 # def next_x(prev_x, iteration)
 #   = x_prev + 2 ** (iteration) * d
 
-def step(prevx, d)
-  prevx += d
-  d *= 2
-
-  [prevx, d]
+def hump?(left, center, right)
+  center > left and
+  center > right
 end
 
 def pit?(left, center, right)
   center < left and
   center < right
+end
+
+def increase?(left, center, right)
+  # (left..right).cover? center
+  left < center and center < right
+end
+
+def decrease?(left, center, right)
+  left > center and center > right
+end
+
+def step(prevx, d)
+  prevx += d
+  d *= 2
+
+  [prevx, d]
 end
 
 def seek(x0, d, f)
@@ -46,28 +60,34 @@ def seek(x0, d, f)
 end
 
 # Svenn method
-def localize(f)
-  x0 = 0  # arbitrary
-  d = 1   # arbitrary, >0
+def localize(f, x0 = 0, d = 1)
+  # x0 = 0  # arbitrary
+  # d = 1   # arbitrary, >0
 
-  f_left = f.(x0 - d)
-  f_center = f.(x0)
-  f_right = f.(x0 + d)
+  # f_left = f.(x0 - d)
+  # f_center = f.(x0)
+  # f_right = f.(x0 + d)
 
-  fail 'f must be unimodal!' if
-    # "hump"
-    f_center > f_left and
-    f_center > f_right
+  range = [x0-d, x0, x0+d]
+  ys = range.map(&f)
+
+  fail 'f must be unimodal!' if hump? *ys
+    # # "hump"
+    # f_center > f_left and
+    # f_center > f_right
 
   # if (f_center < f_left and
       # f_center < f_right) then
 
-  if pit? f_left, f_center, f_right
-    return f_left, f_right
-  end
+  if pit? *ys
+    range.values_at(0, 2)
+    # return f_left, f_right
+  # end
+
+  elsif decrease? *ys
 
   # f is decreasing
-  if (f_right..f_left).cover? f_center
+  # if (f_right..f_left).cover? f_center
     # # k = 1
 
     # # pow2 === 2 ** (k-1)
@@ -94,22 +114,68 @@ def localize(f)
 
     # return x0, x2
 
-    return seek(x0, d, f)
-  elsif (f_left..f_right).cover? f_center
-    return seek(x0, -d, f)
+    seek(x0, d, f)
+  elsif increase? *ys
+    seek(x0, -d, f)
   end
 end
 
 def avg(a, b) = (a + b)/2
 
-def quarter_divide(a, b)
-  xs = [a, b]
-  xs.insert 1, avg(xs[0], xs[-1])
-  xs.insert 1, arg(xs[0], xs[1])
-  xs.insert -2, avg(xs[-2], xs[-1])
+# def insert_avg(arr, at)
+#   a = arr[at-1]
+#   b = arr[at]
+#   c = avg a, b
+#   arr.insert(at, c)
+# end
 
-  xs
+def quarter_divide(a, b)
+  ab = avg(a, b)
+  aab = avg(a, ab)
+  abb = avg(ab, b)
+
+  [a, aab, ab, abb, b]
+
+  # insert_avg xs, 1
+  # insert_avg xs, -1
+  # insert_avg xs, 1
+  # xs.insert 1, avg(xs[0], xs[-1])
+  # xs.insert 1, arg(xs[0], xs[1])
+  # xs.insert -2, avg(xs[-2], xs[-1])
+
+  # xs
 end
 
-def half_divide(a, b)
+def confine_quarters(f, a, b)
   xs = quarter_divide(a, b)
+
+  y3 = xs[1, 3].map(&f)
+
+  indices = case
+    when pit?(*y3) then [1, 3]  # mid
+    when decrease?(*y3) then [2, 4]  # rightmost
+    when increase?(*y3) then [0, 2]  # leftmost
+    end
+
+  xs.values_at *indices
+end
+
+def confine(f, a, b, prec = EPSILON)
+  until b - a <= 2 * prec
+    a, b = confine_quarters(f, a, b)
+  end
+
+  (a + b)/2
+end
+
+# if run as main
+if $PROGRAM_NAME == __FILE__
+  f = proc do |x|
+    arg = (x-2) * x + 11
+    Math.log(arg)
+  end
+
+  a, b = localize f
+
+  p confine f, a, b
+end
