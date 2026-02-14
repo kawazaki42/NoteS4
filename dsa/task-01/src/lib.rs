6a6053137511457c076fa30ecf65b270dadb8634
anonymous <212337579+kawazaki42@users.noreply.github.com>
2026-02-14 13:54:32 +0900

    dsa practice

--- dsa/task-01/src/lib.rs
+++ dsa/task-01/src/lib.rs
@@ -1,5 +1,7 @@
 use rand::distr::Distribution;
 use rand::distr::uniform::{SampleUniform, Uniform};
+use std::io::BufRead;
+use std::num::ParseIntError;
 use std::ops::AddAssign;
 use std::time;
 
@@ -53,6 +55,44 @@ pub fn is_sorted<T: PartialOrd>(arr: &[T]) -> bool {
     arr.windows(2).all(|pair| pair[0] <= pair[1])
 }
 
+pub enum FileError {
+    IoError(std::io::Error),
+    ParseError(ParseIntError),
+}
+
+impl From<std::io::Error> for FileError {
+    fn from(this: std::io::Error) -> Self {
+        Self::IoError(this)
+    }
+}
+
+impl From<ParseIntError> for FileError {
+    fn from(this: ParseIntError) -> Self {
+        Self::ParseError(this)
+    }
+}
+
+pub fn read_arr<T>(path: &std::path::Path) -> Result<Vec<T>, FileError>
+where
+    T: std::str::FromStr<Err = ParseIntError>,
+{
+    let file = std::fs::File::open(path)?;
+    let buf = std::io::BufReader::new(file);
+
+    buf.lines()
+        .flat_map(|r| r.map(|s| s.parse().map_err(Into::into)))
+        .collect()
+    // .filter_map(|r| r.map(|s|))
+    // .map(|r| r.map(|s| s.parse()).flatten())
+    // // .flatten()
+    // .collect()
+    // .map_err(FileError::from)
+
+    //     for l in buf.lines() {
+    //         let n = l.parse().into();
+    //     }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -66,4 +106,6 @@ mod tests {
     fn sorted_error() {
         assert!(!is_sorted(&[1, 2, 3, 4, 4, 5, 6, 7, 6, 7, 6, 7]));
     }
+
+    // TODO: last 2, first 2
 }
