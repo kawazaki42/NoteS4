5257a711cb39f35a7e3a651adc31db953ef7c239
anonymous <212337579+kawazaki42@users.noreply.github.com>
2026-03-07 22:52:51 +0900

    spin off LinAl class

---
+++ comp-mat/14-interpolation/linal.rb
@@ -0,0 +1,108 @@
+# auxilary module to solve linear equation systems
+module LinAl
+  TEST_EQUS = [
+    [5, 2, 9, 2, 44],
+    [8, 8, 2, 6, 54],
+    [7, 2, 8, 9, 71],
+    [3, 8, 8, 3, 55],
+  ]
+
+  refine Numeric do
+    def almost_zero?()
+      self.abs < 1e-10
+    end
+  end
+
+  refine Array do
+    def zeros? = all?(&:almost_zero?)
+  end
+
+  # needed to use `refine` above
+  using self
+
+  # solve an equation system
+  def self.jordan_gauss(mat)
+    nrows = mat.length
+    ncols = mat[0].length
+
+    mat.map! do |row|
+      row.map! &:to_r
+    end
+
+    diaglen = [nrows, ncols].min
+
+    diaglen.times do |k|
+      # pp mat
+      # p
+
+      # step 1
+
+      if mat[k][k].almost_zero?
+        non_zero_beginning = (k+1...nrows).find do |i|
+          not mat[i][k].almost_zero?
+        end
+
+        if non_zero_beginning
+          mat[k], mat[i] = mat[i], mat[k]
+        end
+      end
+
+      # step 2
+
+      leading_item = mat[k][k]
+
+      # (k+1...nrows).each do |i|
+      (0..k-1).chain(k+1...nrows).each do |i|
+        (k+1...ncols).each do |j|
+          mat[i][j] *= leading_item
+          mat[i][j] -= mat[i][k] * mat[k][j]
+          mat[i][j] /= leading_item
+        end
+      end
+
+      # step 3
+
+      # # zero out the column below current element
+      # mat.drop(k+1).each do |row|
+
+      # # zero out the column except current element
+      mat.each_with_index do |row, i|
+        row[k] = 0 if i != k
+      end
+
+      # step 4
+
+      leading_row = mat[k]
+
+      (k...ncols).each do |j|
+        leading_row[j] /= leading_item
+      end
+
+      # step 5 (not thoroughly tested)
+
+      mat.delete_if &:zeros?
+
+      mat.each do |row|
+        if not row[-1].almost_zero? and row[...-1].zeros?
+          pp mat
+          fail 'no solutions possible'
+        end
+      end
+    end
+
+    # extcols = ncols
+    # basic_cols = extcols - 1
+
+    # result = [nil] * basic_cols
+
+    # (0...basic_cols).reverse_each do |k|
+    #   result[k] = mat[k][-1] - (k+1...basic_cols).sum do |j|
+    #     mat[k][j] * result[j]
+    #   end
+    # end
+
+    # result
+
+    mat.map &:last
+  end
+end
