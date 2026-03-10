c7a0ffea0fb5655a574b55310b8ad4fe2eac1418
anonymous <212337579+kawazaki42@users.noreply.github.com>
2026-03-10 13:24:49 +0900

    comp sys org: lec pt 1

--- comp-sys-org/num-base/src/bin/float.rs
+++ comp-sys-org/num-base/src/bin/float.rs
@@ -8,8 +8,24 @@ fn main() {
     // let i = f as u32;
     let i = f.to_bits();
 
-    format!("{i:032b}").chars();
+    // .split_at_checked(4);
+    // .chars()
+    // .collect::<Vec<_>>()
+    // .chunks(4)
+    // .map(|chunk| chunk.iter().collect::<String>())
+    // .collect::<String>();
 
+    let i = grouped(&format!("{i:032b}"));
     println!("{i}");
-    println!("{i:b}");
+    // println!("{i:b}");
+}
+
+fn grouped(s: &str) -> String {
+    match s {
+        "" => String::new(),
+        _ => {
+            let (x, xs) = s.split_at(4);
+            x.to_owned() + " " + &grouped(xs)
+        }
+    }
 }
