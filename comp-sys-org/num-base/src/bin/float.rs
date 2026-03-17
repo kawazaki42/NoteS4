1bcbb6ed36e7b7a66051b0e404f90ad6a6a99d87
anonymous <212337579+kawazaki42@users.noreply.github.com>
2026-03-17 11:04:28 +0900

    comp sys org: task 4

--- comp-sys-org/num-base/src/bin/float.rs
+++ comp-sys-org/num-base/src/bin/float.rs
@@ -5,6 +5,8 @@ fn main() {
 
     let f: f32 = s.trim().parse().unwrap();
 
+    println!("{f:.100}");
+
     // let i = f as u32;
     let i = f.to_bits();
 
@@ -15,17 +17,28 @@ fn main() {
     // .map(|chunk| chunk.iter().collect::<String>())
     // .collect::<String>();
 
-    let i = grouped(&format!("{i:032b}"));
+    let i = grouped_with_sign(&format!("{i:032b}"));
     println!("{i}");
     // println!("{i:b}");
 }
 
+fn grouped_with_sign(s: &str) -> String {
+    // let sign = s[0];
+    format!("{} {}", s.chars().nth(0).unwrap(), grouped(&s[1..]))
+}
+
 fn grouped(s: &str) -> String {
-    match s {
-        "" => String::new(),
-        _ => {
-            let (x, xs) = s.split_at(4);
-            x.to_owned() + " " + &grouped(xs)
-        }
+    // if s.len() < 4 {
+    //     "" => String::new(),
+    //     _ => {
+    //         let (x, xs) = s.split_at(4);
+    //         x.to_owned() + " " + &grouped(xs)
+    //     }
+    // }
+    if s.len() < 4 {
+        s.to_owned()
+    } else {
+        let (x, xs) = s.split_at(4);
+        x.to_owned() + " " + &grouped(xs)
     }
 }
