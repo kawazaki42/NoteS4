8a271b6cc32d8d6828bda7396b968a0f649fd338
anonymous <212337579+kawazaki42@users.noreply.github.com>
2026-04-20 19:31:16 +0900

    oh no i've created a monster

--- dsa/parrot/src/linked.rs
+++ dsa/parrot/src/linked.rs
@@ -1,9 +1,91 @@
-pub enum ListNode<T> {
-    Nil,
-    Cons { value: T, next: Box<ListNode<T>> },
+// pub enum ListNode<T> {
+//     Nil,
+//     Cons { value: T, next: Box<ListNode<T>> },
+// }
+
+// use ListNode::{Cons, Nil};
+
+use std::ops::{Deref, DerefMut};
+
+pub type ListPointer<T> = Option<Box<ListNode<T>>>;
+
+pub struct ListNode<T> {
+    value: T,
+    next: ListPointer<T>,
 }
 
-use ListNode::{Cons, Nil};
+pub trait NakedList<T> {
+    fn is_empty(&self) -> bool;
+    fn len(&self) -> usize;
+    fn contains(&self, elem: &T) -> bool;
+    fn front(&self) -> Option<&T>;
+    fn back(&self) -> Option<&T>;
+}
+
+pub trait MutableNakedList<T>: NakedList<T> {
+    fn front_mut(&mut self) -> Option<&mut T>;
+}
+
+impl<T, P> NakedList<T> for Option<P>
+where
+    P: Deref<Target = ListNode<T>>,
+{
+    // TODO: new/default
+
+    fn is_empty(&self) -> bool {
+        match self {
+            None => true,
+            _ => false,
+        }
+    }
+
+    fn len(&self) -> usize {
+        match self {
+            None => 0,
+            Some(cur) => 1 + cur.next.len(),
+        }
+    }
+
+    fn contains(&self, elem: &T) -> bool {
+        match self {
+            None => false,
+            Some(cur) => cur.next.contains(elem),
+        }
+    }
+
+    fn front(&self) -> Option<&T> {
+        match self {
+            None => None,
+            Some(cur) => Some(&cur.value),
+        }
+
+        // self.map(|cur| &cur.value)
+    }
+
+    fn back(&self) -> Option<&T> {
+        match self {
+            None => None,
+            Some(cur) => match cur.next {
+                None => Some(&cur.value),
+                Some(next) => next.back(),
+            },
+        }
+    }
+}
+
+impl<T, P> MutableNakedList<T> for Option<P>
+where
+    P: DerefMut<Target = ListNode<T>>,
+{
+    fn front_mut(&mut self) -> Option<&mut T> {
+        match self {
+            None => None,
+            Some(cur) => Some(&mut cur.value),
+        }
+    }
+
+    fn bad
+}
 
 pub struct SinglyLinkedList<T> {
     head: ListNode<T>,
