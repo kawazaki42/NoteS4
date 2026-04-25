b4ef4b55ff2831ed1541fb1cb2ccf6aed388b4b8
anonymous <212337579+kawazaki42@users.noreply.github.com>
2026-04-25 23:06:55 +0900

    i failed to make a iter impl...

--- dsa/test-02-sort/src/lib.rs
+++ dsa/test-02-sort/src/lib.rs
@@ -48,25 +48,136 @@ pub fn is_sorted<T: PartialOrd>(arr: &[T]) -> bool {
     true
 }
 
-struct Merge<'a, T>(&'a [T], &'a [T]);
+enum CachedIter<T, I>
+where
+    I: Iterator<Item = T>,
+{
+    Nil,
+    Cons(T, I),
+}
 
-impl<'a, T: PartialOrd> Iterator for Merge<'a, T> {
+// impl<T> FromIterator<T> for CachedIter<T, IntoIterator<Item = T>::IntoIter> {
+//     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {}
+// }
+
+// impl<T, I> CachedIter<T, I>
+// where
+//     I: Iterator<Item = T>,
+// {
+//     pub fn split_first(&mut self) -> Option<T> {
+//         match std::mem::replace(self, Nil) {
+//             Nil => None,
+//             Cons(head, tail) => {
+//                 *self = tail.into();
+
+//                 Some(head)
+//             }
+//         }
+//     }
+// }
+
+impl<T, I> From<I> for CachedIter<T, I::IntoIter>
+where
+    I: IntoIterator<Item = T>,
+{
+    fn from(value: I) -> Self {
+        let mut iter = value.into_iter();
+
+        if let Some(head) = iter.next() {
+            Cons(head, iter)
+        } else {
+            Nil
+        }
+    }
+}
+
+use CachedIter::*;
+
+struct MergeIter<T, I>(CachedIter<T, I>, CachedIter<T, I>)
+where
+    I: Iterator<Item = T>;
+
+// impl<T, I, C> Merge<T, I>
+// where
+//     I: Iterator<Item = T>,
+//     C: IntoIterator,
+// {
+//     pub fn new(iter1: C, iter2: C) {
+
+//     }
+// }
+
+impl<T, I> Iterator for MergeIter<T, I>
+where
+    I: Iterator<Item = T>,
+    T: PartialOrd,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let owned = std::mem::replace(self, Self(Nil, Nil));
+
+        match owned {
+            MergeIter(Nil, Nil) => None,
+            MergeIter(Cons(head0, tail0), Cons(head1, tail1)) => {
+                if head0 <= head1 {
+                    *self = Self(tail0.into(), Cons(head1, tail1));
+                    Some(head0)
+                } else {
+                    *self = Self(Cons(head0, tail0), tail1.into());
+                    Some(head1)
+                }
+            }
+            MergeIter(Cons(head, tail), Nil) | MergeIter(Nil, Cons(head, tail)) => {
+                *self = Self(tail.into(), Nil);
+                Some(head)
+            }
+        }
+
+        // let head0 = self.0.split_first();
+        // let head1 = self.1.split_first();
+
+        // match (pair0, pair1) {
+        //     (None, None) => None,
+        //     (Some(a), Some(b)) => {
+        //         if head0 <= head1 {
+        //             self.0 = tail0;
+        //             Some(head0)
+        //         } else {
+        //             self.1 = tail1;
+        //             Some(head1)
+        //         }
+        //     }
+        //     (Some(pair), None) | (None, Some(pair)) => {
+        //         let (head, tail) = pair;
+        //         self.0 = tail;
+        //         self.1 = Nil;
+
+        //         Some(head)
+        //     }
+        // }
+    }
+}
+
+struct MergeSlice<'a, T>(&'a [T], &'a [T]);
+
+impl<'a, T: PartialOrd> Iterator for MergeSlice<'a, T> {
     type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
         match self {
-            Merge([], []) => None,
-            Merge([], [head, tail @ ..]) => {
+            MergeSlice([], []) => None,
+            MergeSlice([], [head, tail @ ..]) => {
                 self.1 = tail;
 
                 Some(head)
             }
-            Merge([head, tail @ ..], []) => {
+            MergeSlice([head, tail @ ..], []) => {
                 self.0 = tail;
 
                 Some(head)
             }
-            Merge([head1, tail1 @ ..], [head2, tail2 @ ..]) => {
+            MergeSlice([head1, tail1 @ ..], [head2, tail2 @ ..]) => {
                 if head1 <= head2 {
                     self.0 = tail1;
 
@@ -83,8 +194,32 @@ impl<'a, T: PartialOrd> Iterator for Merge<'a, T> {
 
 /// `a` and `b` must be sorted!
 fn merge<T: PartialOrd + Clone>(a: &[T], b: &[T]) -> Vec<T> {
-    Merge(a, b).cloned().collect()
+    MergeSlice(a, b).cloned().collect()
 }
+
+fn merge_iter<T, I>(a: I, b: I) -> MergeIter<T, <I as IntoIterator>::IntoIter>
+where
+    T: PartialOrd,
+    I: IntoIterator<Item = T>,
+{
+    MergeIter(a.into(), b.into())
+}
+
+// pub fn merge_sort_iter<T, I>(iter: I) -> MergeIter<T, I>
+// where
+//     T: PartialOrd,
+//     // I: IntoIterator<Item = T>,
+//     I: ExactSizeIterator + Iterator<Item = T>,
+// {
+//     let len = iter.len();
+//     let a = iter.into_iter().take(len / 2);
+//     let b = iter.into_iter().skip(len / 2);
+
+//     let a = merge_sort_iter(a);
+//     let b = merge_sort_iter(b);
+
+//     merge_iter(a, b)
+// }
 
 pub fn merge_sort<T: Clone + PartialOrd>(arr: &[T]) -> Vec<T> {
     match arr {
