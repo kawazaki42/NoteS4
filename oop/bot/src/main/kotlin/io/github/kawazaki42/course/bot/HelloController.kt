1df7e1691a5650e9e79db409c4ff0351fe083b24
anonymous <212337579+kawazaki42@users.noreply.github.com>
2026-05-04 23:43:32 +0900

    HUGE amount of work.
    - LLM chat now really works and shows messages,
      not JSON objects
    - Ellipses (`...`) seem to work correctly
      (from like the 2nd attempt, isn't it suspicious?)
    - STREAMING WORKS WOOHOO!

--- oop/bot/src/main/kotlin/io/github/kawazaki42/course/bot/HelloController.kt
+++ oop/bot/src/main/kotlin/io/github/kawazaki42/course/bot/HelloController.kt
@@ -1,33 +1,70 @@
 package io.github.kawazaki42.course.bot
 
+import javafx.application.Platform
+import javafx.collections.FXCollections.observableArrayList
 import javafx.fxml.FXML
 import javafx.scene.control.Label
+import javafx.scene.control.Labeled
 import javafx.scene.control.ListCell
 import javafx.scene.control.ListView
 import javafx.scene.control.TextArea
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyEvent
 import javafx.util.Callback
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import kotlinx.serialization.json.Json
 import java.net.URI
-import java.net.URL
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
-import java.net.http.HttpRequest.BodyPublishers
 import java.net.http.HttpResponse
+import java.util.stream.Stream
+import kotlin.concurrent.thread
 
 //import java.awt.event.KeyEvent
+
+@Serializable
+enum class Role {
+    system,
+    user,
+    assistant,
+    tool,
+}
+
+@Serializable
+class Message(var role: Role, var content: String, @Transient var complete: Boolean = true) {
+    override fun toString() = if (complete) content else "$content..."
+}
+
+@Serializable
+class OllamaRequest(val model: String, var messages: List<Message>) {
+//    fun sendPrompt(prompt: String, messages: ObservableList<Message>) {
+//    }
+}
+
+@Serializable
+data class OllamaResponse(val message: Message)
+
+//class CustomListCell<T>: ListCell<T>() {
+//    init {
+//        isWrapText = true
+//    }
+//}
 
 class HelloController {
     @FXML
     private lateinit var welcomeText: Label
 
-    val dialog = mutableListOf<String>()
+//    @FXML
+//    private val dialog = FXCollections.observableArrayList(Message(Role.system,
+//        "answer briefly. i'm testing my own implementation of llm frontend"
+//    ))
 
     @FXML
-    private lateinit var dialogView: ListView<String>
+    private lateinit var dialogView: ListView<Message>
 
     @FXML
-    private lateinit var prompt: TextArea
+    private lateinit var promptInput: TextArea
 
     @FXML
     private fun onKeyTyped() {}
@@ -41,11 +78,83 @@ class HelloController {
 //        }
         if (key.code == KeyCode.ENTER) {
             if (key.isShiftDown)
-                prompt.text += '\n'
+                promptInput.text += '\n'
             else
                 sendPrompt()
         }
     }
+
+    var model = OllamaRequest("gemma4:31b-cloud", emptyList())
+
+    fun initialize() {
+        dialogView.items = observableArrayList(Message(Role.system,
+            "answer briefly. i'm testing my own implementation of llm frontend",
+            complete = true
+        ))
+
+        model.messages = dialogView.items
+
+
+//        dialogView.cellFactory = Callback { _: ListView<Message> -> CustomListCell<Message>() }
+    }
+
+    val jsonDecoder = Json {
+        ignoreUnknownKeys = true
+    }
+
+    var incompleteMessage: Message? = null
+
+    val client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build()
+
+    class HttpStatusNotOk(val code: Int): Exception("HTTP Status $code")
+
+//        private fun sendRequest(incomplete: Message): Result<HttpResponse<Stream<String>>> {
+        private fun sendRequest(incomplete: Message): HttpResponse<Stream<String>> {
+        val uri = URI.create("http://localhost:11434/api/chat/")
+
+//        val stringPublisher: (String) -> HttpRequest.BodyPublisher = HttpRequest.BodyPublishers::ofString
+
+//        dialogView.items.
+
+//        model.messages += Message(Role.user, question)
+        val jsonRequest = Json.encodeToString(model)
+
+//        val bodyHandler = HttpResponse.BodyHandlers.ofString()
+//        val bodyPublisher = BodyPublishers.ofString("""{
+//            "model": "gemma4:31b-cloud",
+//            "messages": [{"role": "user", "content": "$question"}]
+//        }""")
+        val bodyPublisher = HttpRequest.BodyPublishers.ofString(jsonRequest)
+        val bodyHandler = HttpResponse.BodyHandlers.ofLines()
+
+        val request = HttpRequest.newBuilder(uri).POST(bodyPublisher).build()
+
+//        Platform.runLater {
+        //        try {
+//        val response = client.runCatching{ send(request, bodyHandler) }.getOrElse { e ->
+//            //        } catch (e: Exception) {
+//            return Message(Role.system, "<Error: $e>")
+//        }
+
+            // throws
+        val response = client.send(request, bodyHandler)
+
+        if (response.statusCode() != 200)
+            throw HttpStatusNotOk(response.statusCode())
+
+//            val new = Message(Role.assistant, "")
+            incomplete.role = Role.assistant
+//            incompleteMessage = new
+
+            //            model.messages += new
+//            messages += incompleteMessage
+
+        //            model.messages += Message(Role.system, "<Error ${response.statusCode()}")
+//            messages += Message(Role.system, "<Error ${response.statusCode()}>")
+
+            return response
+
+        }
 
     @FXML
     private fun sendPrompt() {
@@ -62,8 +171,77 @@ class HelloController {
 
 //        println(question.style)
 
-        val question = prompt.text
-        prompt.clear()
+        val prompt = promptInput.text
+        promptInput.clear()
+
+//        Platform.runLater {
+//            model.sendPrompt(question, dialogView.items)
+////            dialogView.refresh()
+//        }
+
+        val messages = dialogView.items
+        messages += Message(Role.user, prompt, complete = true)
+
+
+    //        val response = object: HttpResponse<Sequence<String>> {
+    //            val mockLines = listOf(
+    //                """{"message": {"role": "assistant", "content": "pi"}}""",
+    //                """{"message": {"role": "assistant", "content": "vo"}}""",
+    //            )
+    //            override fun body() = mockLines.asSequence()
+    //            override fun headers() = null
+    //            override fun previousResponse() = null
+    //            override fun request() = null
+    //            override fun sslSession() = null
+    //            override fun statusCode() = 200
+    //            override fun uri() = null
+    //            override fun version() = null
+    //        }
+
+    //        val answer = if (response.statusCode() != 200)
+    //            "<http error ${response.statusCode()}>"
+    //        else {
+    //            response.body()
+    //        }
+
+    //        dialogView.items.
+
+        val new = Message(Role.system, "", complete = false)
+        messages += new
+
+                thread {
+                    val response = runCatching {
+                        sendRequest(new)
+                    }.getOrElse { e ->
+                        new.content = "<Error: $e>"
+                        new.complete = true
+                        return@thread
+                    }
+//                    try {
+//                        response =
+//                    } catch (e: Exception) {
+//                        new.content = "<Error: $e>"
+//                    }
+
+                    for (line in response.body()) {
+                        val r: OllamaResponse = jsonDecoder.decodeFromString(line)
+                        Platform.runLater {
+                            new.content += r.message.content
+                            dialogView.refresh()
+                        }
+                    }
+                    new.complete = true
+                    dialogView.refresh()
+
+                }
+
+    //            dialogView.refresh()
+    //            dialogView.childrenUnmodifiable.last()
+
+
+        }
+
+
 //        val answer = "pivo"
 //
 //        dialog.add(question)
@@ -79,34 +257,20 @@ class HelloController {
 
 //        dialogView.cellFactory = fac
 
-        val client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build()
+//        model.messages = dialogView.items
 
-        val uri = URI.create("http://localhost:11434/api/chat/")
-
-//        val stringPublisher: (String) -> HttpRequest.BodyPublisher = HttpRequest.BodyPublishers::ofString
-
-        val bodyHandler = HttpResponse.BodyHandlers.ofString()
-        val bodyPublisher = BodyPublishers.ofString("""{
-            "model": "gemma4:31b-cloud",
-            "messages": [{"role": "user", "content": "$question"}]
-        }""")
-
-        val request = HttpRequest.newBuilder(uri).POST(bodyPublisher).build()
-
-        val response = client.send(request, bodyHandler)
-
-        dialogView.items += question
-        val msg = """
-            ${response.toString()}
-            
-            ${response.body()}
-            
-            ${response.uri()} ${response.statusCode()} ${response.version()}
-        """.trimIndent()
+//        dialogView.items += question
+//        val msg = """
+//            ${response.toString()}
+//
+//            ${response.body()}
+//
+//            ${response.uri()} ${response.statusCode()} ${response.version()}
+//        """.trimIndent()
 //        dialogView.items += response.statusCode().toString()
 //        dialogView.items += response.toString()
-        dialogView.items += msg
+//        dialogView.items += msg
 
 //        println(dialogView.cssMetaData)
-    }
+//    }
 }
--- oop/bot/src/main/kotlin/module-info.java
+++ oop/bot/src/main/kotlin/module-info.java
@@ -4,7 +4,9 @@ module io.github.kawazaki42.course.bot {
     requires kotlin.stdlib;
 
     requires java.net.http;
-
+    requires kotlinx.serialization.json;
+//    requires kotlin.maven.serialization;
+    requires kotlinx.serialization.core;
 
     opens io.github.kawazaki42.course.bot to javafx.fxml;
     exports io.github.kawazaki42.course.bot;
