453fe0981c85efd118441b72a6fae62bab092119
anonymous <212337579+kawazaki42@users.noreply.github.com>
2026-06-05 00:52:58 +0900

    lab 4.5

--- db/lab/select_complex.sql
+++ db/lab/select_complex.sql
@@ -1,20 +1,20 @@
 -- вычислить год по номеру семестра и году начала обучения
-CREATE OR REPLACE FUNCTION year(
+CREATE OR REPLACE FUNCTION year_from_semester(
   begin SMALLINT,
   semester SMALLINT
 ) RETURNS INTEGER
 LANGUAGE 'sql'
 AS 'SELECT begin + semester/2';
 
--- суммарная нагрузка преподавателя в каждом году
+-- суммарная нагрузка преподавателя в каждом году (4.1)
 CREATE OR REPLACE VIEW lecturer_year_hours AS
 SELECT DISTINCT
   lecturer.id AS id_lecturer,
   lecturer.surname,
   lecturer.first_name,
   lecturer.patronym,
-  year("group".begin_year, lesson.semester_relative) AS year,
-  SUM(standard.study_hours)
+  year_from_semester("group".begin_year, lesson.semester_relative) AS year,
+  SUM(standard.study_hours) AS hours
 FROM lesson
   JOIN lecturer ON id_lecturer = lecturer.id
   JOIN "group" ON name_group = "group".name
@@ -30,7 +30,7 @@ SELECT
   lecturer.surname,
   lecturer.first_name,
   lecturer.patronym,
-  year("group".begin_year, lesson.semester_relative) AS year,
+  year_from_semester("group".begin_year, lesson.semester_relative) AS year,
   "group".name AS "group",
   discipline.name as discipline,
   standard.name_kind as kind,
@@ -42,7 +42,7 @@ FROM lesson
   JOIN discipline ON id_discipline = discipline.id;
 
 
--- среднее количество лекций и лабораторных (по часам)
+-- среднее количество лекций и лабораторных (по часам) (4.2)
 CREATE OR REPLACE VIEW avg_lecs_labs_by_degree AS
 SELECT
   degree,
@@ -52,3 +52,106 @@ FROM lesson
   JOIN standard ON id_standard = standard.id
   WHERE kind IN ('лекция', 'лабораторная')
   GROUP BY degree;
+
+CREATE TEMPORARY TABLE seniority_range(
+    begin SMALLINT,
+    "end" SMALLINT
+);
+
+INSERT INTO seniority_range VALUES
+(6, 7),
+(5, 10),
+(10, 15);
+
+SELECT
+  lecturer.id AS id_lecturer,
+  lecturer.surname,
+  lecturer.first_name,
+  lecturer.patronym,
+  r.begin,
+  r.end,
+  SUM(study_hours)
+FROM lesson
+  JOIN lecturer ON id_lecturer = lecturer.id
+  JOIN standard ON id_standard = standard.id
+  JOIN seniority_range AS r ON EXTRACT(YEAR FROM CURRENT_DATE) - lecturer.work_begin_year BETWEEN r.begin AND r."end"
+  GROUP BY lecturer.id, r.begin, r."end";
+
+-- CREATE OR REPLACE FUNCTION hours_by_seniority(
+--   begin SMALLINT,
+--   end SMALLINT
+-- ) RETURNS
+
+-- import
+CREATE EXTENSION tablefunc;
+
+SELECT
+  id_lecturer,
+  l.surname,
+  l.first_name,
+  l.patronym,
+  year1, year2, year3
+FROM CROSSTAB(
+  $$
+  SELECT
+    id_lecturer AS row_name,
+    year AS col_name,
+    hours::INTEGER AS cell
+  FROM lecturer_year_hours
+    ORDER BY year DESC
+  $$
+  -- $$
+  -- SELECT year FROM lecturer_year_hours ORDER BY year;
+  -- $$
+) AS (id_lecturer INTEGER, year1 INTEGER, year2 INTEGER, year3 INTEGER)
+JOIN lecturer AS l ON id_lecturer = l.id
+ORDER BY 1;
+
+
+SELECT *
+  -- id_lecturer,
+  -- l.surname,
+  -- l.first_name,
+  -- l.patronym,
+  -- year1, year2, year3
+  --
+FROM CROSSTAB(
+  $$
+  SELECT
+    id_lecturer AS row_name,
+    surname, first_name, patronym,
+    year AS col_name,
+    hours::INTEGER AS cell
+  FROM lecturer_year_hours
+    ORDER BY 1
+  $$,
+  $$
+  SELECT DISTINCT year FROM lecturer_year_hours ORDER BY year DESC LIMIT 3
+  $$
+) AS (id_lecturer INTEGER, surname TEXT, first_name TEXT, patronym TEXT, year1 INTEGER, year2 INTEGER, year3 INTEGER)
+-- JOIN lecturer AS l ON id_lecturer = l.id
+ORDER BY 1;
+
+
+-- SELECT
+--   id_lecturer,
+--   -- l.surname,
+--   -- l.first_name,
+--   -- l.patronym,
+--   year1, year2, year3
+-- FROM CROSSTAB(
+--   $$
+--   SELECT
+--     id_lecturer::INTEGER AS row_name,
+--     -- surname, first_name, patronym,
+--     year::INTEGER AS col_name,
+--     hours::INTEGER AS cell
+--   FROM lecturer_year_hours
+--     ORDER BY 1
+--   $$,
+--   $$
+--   SELECT DISTINCT year FROM lecturer_year_hours ORDER BY year DESC LIMIT 3
+--   $$
+-- ) AS (id_lecturer INTEGER, year1 INTEGER, year2 INTEGER, year3 INTEGER)
+-- -- JOIN lecturer AS l ON id_lecturer = l.id
+-- ORDER BY 1;
